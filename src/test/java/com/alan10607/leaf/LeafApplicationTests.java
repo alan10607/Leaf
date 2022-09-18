19b313ec62447cfb1dacdf8a224174f52a66aa52
alan10607 <hiden1414@gmail.com>
2022-09-18 16:17:54 +0800

    update docker

--- src/test/java/com/alan10607/leaf/LeafApplicationTests.java
+++ src/test/java/com/alan10607/leaf/LeafApplicationTests.java
@@ -1,49 +1,13 @@
 package com.alan10607.leaf;
 
-import com.alan10607.leaf.service.ViewService;
-import com.mysql.cj.xdevapi.AbstractFilterParams;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-
-
 class LeafApplicationTests {
-	@Autowired
-	private RedissonClient redisson;
 	
 	@Test
 	void contextLoads() {
-		RLock lock = redisson.getLock("testlock");
-		try {
-			lock.lock();
-			Thread.sleep(1000);
-		}catch (Exception e){
-
-		}finally {
-			lock.unlock();
-			System.out.println("unlock, trylock:" + lock.tryLock());
-		}
-
-
-		if(lock.tryLock()){
-			try {
-				lock.lock();
-				Thread.sleep(1000);
-			}catch (Exception e){
-
-			}finally {
-				lock.unlock();
-				System.out.println("unlock, trylock:" + lock.tryLock());
-			}
-		}else{
-			System.out.println("already locked, trylock:" + lock.tryLock());
-		}
 	}
 
 }
